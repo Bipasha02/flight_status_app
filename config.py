481ab2dd2a1545c23e940ab91871e3e0dd52956b
Bipasha Kumar <79408081+Bipasha02@users.noreply.github.com>
2024-07-30 20:07:23 +0530

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class Config:
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://user/backend/flightstatus'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.environ.get('SECRET_KEY')
